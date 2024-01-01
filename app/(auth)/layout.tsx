30b2ff8f0f920ccd6cbb59ff56463927f98e7c37
Ayaz <ayazumman@gmail.com>
2024-01-01 11:14:18 +0400

    first

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  );
+}
